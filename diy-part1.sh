b268138d0e2acc8623dde5529d1d66f6db34e071
YeLing <ylls520@qq.com>
2025-04-07 10:14:01 +0800

    Delete diy-part1.sh

--- diy-part1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Add a feed source
-echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
-#echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
-
-#添加额外软件包
