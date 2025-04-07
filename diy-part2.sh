0f9ce469cf5497dd157b13c962aa789caa8693e9
YeLing <ylls520@qq.com>
2025-04-07 10:06:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,9 +3,3 @@
 #添加自定义配置
 cat $GITHUB_WORKSPACE/config/kmod_config >> .config
 cat $GITHUB_WORKSPACE/config/luci_config >> .config
-
-#删除冲突的软件包
-#rm -rf ./package/istore
-rm -rf ./feeds/kenzo/luci-app-quickstart
-rm -rf ./feeds/kenzo/luci-app-store
-rm -rf ./feeds/kenzo/luci-lib-taskd
