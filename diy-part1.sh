32932c2666c14d847e51f34aa09f4437ad51a72b
YeLing <ylls520@qq.com>
2025-04-07 09:50:35 +0800

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Add a feed source
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
+#echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
+
+#添加额外软件包
