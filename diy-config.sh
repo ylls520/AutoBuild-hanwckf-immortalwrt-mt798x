cceacf1e1d629ef0d7b1c0281ef3229b8a9a16a7
YeLing <ylls520@qq.com>
2025-04-25 19:02:39 +0800

    Update diy-config.sh

--- diy-config.sh
+++ diy-config.sh
@@ -2,6 +2,8 @@
 
 # 拷贝默认配置
 cp -f defconfig/$CONFIG_FILE .config
+echo 'CONFIG_TARGET_DEVICE_mediatek_mt7986_DEVICE_jdcloud_re-cp-03=y' > .config
+echo 'CONFIG_TARGET_DEVICE_PACKAGES_mediatek_mt7986_DEVICE_jdcloud_re-cp-03=""' > .config
 
 # 添加自定义配置
 cat $GITHUB_WORKSPACE/config/kmod_config >> .config
